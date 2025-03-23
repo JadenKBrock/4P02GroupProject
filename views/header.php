b1aeb8e4df87aae67471e5797479fe6d8f2b4013
XPosterMaloneX <47110015+XPosterMaloneX@users.noreply.github.com>
2025-03-23 11:45:12 -0400

    Update header.php

--- views/header.php
+++ views/header.php
@@ -44,8 +44,8 @@ function isActive($page) {
                     </div>
                 </div>
             <?php else: ?>
-                <a href="login.php" class="nav-btn">Login</a>
-                <a href="register.php" class="nav-btn">Register</a>
+                <a href="src/Login/login_page.php" class="nav-btn">Login</a>
+                <a href="src/Register/register_page.php" class="nav-btn">Register</a>            
             <?php endif; ?>
         </div>
     </nav>

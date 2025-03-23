8772ec9dd2c5a4c7b06246e392d0c709d200e232
ThomasSemenak <157653726+ThomasSemenak@users.noreply.github.com>
2025-03-22 22:13:40 -0400

    Update header.php

--- views/header.php
+++ views/header.php
@@ -1,5 +1,5 @@
 <?php
-//$base_url = "http://localhost:8080/";
+session_start(); // Start the session if it's not already started
 $base_url = "https://" . $_SERVER['HTTP_HOST'] . "/";
 
 function isActive($page) {
@@ -21,7 +21,6 @@ function isActive($page) {
             }
         }
     ?>
-
     <script type='text/javascript' src='https://platform-api.sharethis.com/js/sharethis.js#property=67d6417b86189a0019fafc7a&product=sop' async='async'></script>
 </head>
 <body>
@@ -33,7 +32,23 @@ function isActive($page) {
         <a href="<?php echo $base_url;?>src/Generate/generate_page.php" class="<?php echo isActive('generate_page.php');?>">Generate</a>
         <a href="<?php echo $base_url;?>index.php" class="<?php echo isActive('about_us.php');?>">About Us</a>
         <a href="<?php echo $base_url;?>index.php" class="<?php echo isActive('faq.php');?>">FAQ</a>
-        <a href="<?php echo $base_url;?>src/Login/login_pageNew.php" class="<?php echo isActive('login_pageNew.php');?>">Login</a>
-        <a href="<?php echo $base_url;?>src/Register/register_pageNew.php" class="<?php echo isActive('register_pageNew.php');?>">Sign Up</a>
+        
+        <?php if (isset($_SESSION['user_id'])): ?>
+            <!-- If user is logged in, show a profile circle with a dropdown for Logout -->
+            <div class="profile-menu">
+                <div class="profile-icon">
+                    <?php 
+                        // Display the first letter of the username if available, else a default letter
+                        echo isset($_SESSION['username']) ? strtoupper(substr($_SESSION['username'], 0, 1)) : "U"; 
+                    ?>
+                </div>
+                <div class="dropdown-menu">
+                    <a href="<?php echo $base_url; ?>logout.php">Logout</a>
+                </div>
+            </div>
+        <?php else: ?>
+            <a href="<?php echo $base_url;?>src/Login/login_pageNew.php" class="<?php echo isActive('login_pageNew.php');?>">Login</a>
+            <a href="<?php echo $base_url;?>src/Register/register_pageNew.php" class="<?php echo isActive('register_pageNew.php');?>">Sign Up</a>
+        <?php endif; ?>
     </nav>
 </div>

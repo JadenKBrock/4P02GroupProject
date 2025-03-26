c67a1cc3733ca9f56124f25b1a2399b1aa471b0e
JadenKBrock <jk21pf@brocku.ca>
2025-03-26 15:58:57 -0400

    Changed Generate page a bit

--- src/Generate/generate_page.php
+++ src/Generate/generate_page.php
@@ -1,34 +1,54 @@
 <?php
 $page_title = "Generate";
-$page_styles = [""];
+$page_styles = ["generate.css"];
 include "../../views/header.php";
 ?>
 
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
-<h2>Testing LLM</h2>
-<label for="contentType">Content Type:</label>
-<input type="text" id="contentType">
-<br><br>
-<label for="contentText">Content Text:</label>
-<input type="text" id="contentText">
-<br><br>
-<button id="sendRequest">Get Response</button>
+<div class="generate-container">
+    <div class="input-outer-container">
+        <h3>Generate Post</h3>
+        <div class="format-container">
+            <div class="dropdown-container">
+                <button onclick="dropdown()" class="formatbtn">Format</button>
+                <div id="dropdown" class="dropdown-content">
+                    <button onclick="selectFormat('Facebook')">Facebook</button>
+                    <button onclick="selectFormat('Twitter')">Twitter</button>
+                    <button onclick="selectFormat('Email')">Email</button>
+                </div>
+            </div>
+            <p id="format-selection">Facebook</p>
+        </div>
+        <div class="input-container">
+            <input type="text" id="userInput" placeholder="Enter a topic or keyword.">
+            <button id="sendRequest">Generate</button>
+        </div>
+    </div>
+    <div class="output_container">
+        
+    </div>
+</div>
 
 <p>Response: <span id="responseText"></span></p>
 
 <script>
+
+function selectFormat(format) {
+    $("#format-selection").text(format);
+}
+
 $(document).ready(function() {
     $("#sendRequest").click(function() {
-        var contentType = $("#contentType").val();  // Get content type
-        var contentText = $("#contentText").val();  // Get content text
+        var format = $("#format-selection").val();  // Get content type
+        var userInput = $("#userInput").val();  // Get content text
 
         $.ajax({
             url: "run_generate_py.php",  // Calls updated PHP script
             type: "POST",
             data: JSON.stringify({ 
-                content_type: contentType, 
-                keyword: contentText 
+                format_type: format, 
+                keyword: userInput 
             }),
             contentType: "application/json",
             dataType: "json",  // Ensure response is treated as JSON

5ac02d2dcca0f4ca106c1b32dbb9eca10417056b
XPosterMaloneX <47110015+XPosterMaloneX@users.noreply.github.com>
2025-04-07 19:29:29 -0400

    Update header.php

--- views/header.php
+++ views/header.php
@@ -45,7 +45,6 @@ function isActive($page) {
             <a href="<?php echo $base_url;?>src/Login/login_pageNew.php" class="<?php echo isActive('login_pageNew.php');?>">Login</a>
             <a href="<?php echo $base_url;?>src/Register/register_pageNew.php" class="<?php echo isActive('register_pageNew.php');?>">Register</a>
         <?php else: ?>
-            echo "<!-- Logged in as user ID: " . $_SESSION['user_id'] . " -->";
             <a href="<?php echo $base_url;?>src/Profile/profile_page.php" class="<?php echo isActive('profile_page.php');?>">Profile</a>
             <a href="<?php echo $base_url;?>logout.php" class="nav-btn" style="margin-left: 10px; color: red;">Logout</a>
         <?php endif; ?>

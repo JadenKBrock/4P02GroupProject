b909a49f5a57474981f77b0e0158787768555c26
JadenKBrock <jk21pf@brocku.ca>
2025-04-08 11:40:03 -0400

    t

--- index.php
+++ index.php
@@ -5,6 +5,10 @@
 
 <?php
 //ob_start();
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Pragma: no-cache");
+header("Expires: 0");
+
 session_set_cookie_params([
   'lifetime' => 0,
   'path' => '/',
